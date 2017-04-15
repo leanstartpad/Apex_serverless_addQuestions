a12fe0dda9a64341bceefc1c97b396388962c605
viveksharma619 <s.vivekblog@gmail.com>
2017-04-16 00:48:48 +0530

    Added params for post

--- functions/hello/index.js
+++ functions/hello/index.js
@@ -2,14 +2,20 @@ const AWS = require('aws-sdk');
 const docClient = new AWS.DynamoDB.DocumentClient({region: 'us-west-2'});
 
 exports.handle = function(e, ctx, cb) {
-  var params = {
-    Item:{
-      question_number: 0,
-      question_text: 'What is HTML ?',
-      answer: 'HTML stands for Hypertext Markup Language',
-      app_name: 'HTML'
-    },
-    TableName: 'interview_questions'
+  if(e.question_number && e.question_text && e.answer && e.app_name){
+    var params = {
+      Item:{
+        question_number: e.question_number,
+        question_text: e.question_text,
+        answer: e.answer,
+        app_name: e.app_name,
+        question_description: e.question_description || null
+      },
+      TableName: 'interview_questions'
+    }
+  }
+  else{
+    cb(new Error('Data Incomplete'),null);
   }
 
   docClient.put(params, function(err, data){
